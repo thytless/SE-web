814926cdc035902269871d047d7386afd21f4145
Ginnna <2569500918@qq.com>
2018-05-27 20:03:09 +0800

    add

---
+++ server/src/main/java/com/seweb/backend/domain/ContactUs.java
@@ -0,0 +1,5 @@
+package com.seweb.backend.domain;
+
+public class ContactUs {
+
+}
--- server/src/main/java/com/seweb/backend/domain/Text.java
+++ server/src/main/java/com/seweb/backend/domain/Text.java
@@ -1,6 +1,6 @@
 package com.seweb.backend.domain;
 
-public class Text {
+public class Text extends BaseEntity{
     private String content;
 
     public String getContent() {
