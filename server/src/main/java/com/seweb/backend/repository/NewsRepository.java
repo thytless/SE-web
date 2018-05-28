810b865e153a1528c5389c2d77112a33149c56c4
thytless <316547941@qq.com>
2018-05-28 06:30:35 -0700

    add annotations to fix bugs

--- server/src/main/java/com/seweb/backend/repository/NewsRepository.java
+++ server/src/main/java/com/seweb/backend/repository/NewsRepository.java
@@ -1,6 +1,7 @@
 package com.seweb.backend.repository;
 
-import com.seweb.backend.domain.News;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NewsRepository extends TextRepository {
 }
--- server/src/main/java/com/seweb/backend/repository/TextRepository.java
+++ server/src/main/java/com/seweb/backend/repository/TextRepository.java
@@ -1,7 +1,11 @@
 package com.seweb.backend.repository;
 
 import com.seweb.backend.domain.Text;
+import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.stereotype.Component;
 
+@Component(value = "textRepository")
+@NoRepositoryBean
 public interface TextRepository extends BaseRepository<Text>{
 
 }
